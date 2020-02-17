ffc510afc1d0d309b4216302af9e0ce11ceccad5
aubrey-y <aubreym.yan@gmail.com>
2020-02-17 00:13:21 -0500

    started frontend

--- autostock/views/demo.py
+++ autostock/views/demo.py
@@ -1,11 +1,13 @@
+from flask import render_template
+
 from autostock import app
 
 
 @app.route("/")
-def home():
-    return "hello."
+def index():
+    return render_template("index.html")
 
 
-@app.route('/check')
-def test_config():
-    return app.config["ROBIN_HOOD_PASS"]
+@app.route('/<string:page_name>/')
+def render_static(page_name):
+    return render_template('%s.html' % page_name)
